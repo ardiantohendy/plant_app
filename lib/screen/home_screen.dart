9023f4742d57b908311f28f65a16d8e33ae8811a
Ardiantohendy <bayuardianto421@gmail.com>
2023-06-02 00:11:36 +0700

    new commit, and it's excited me hehe

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -167,6 +167,7 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
                 child: TabBarView(
                   controller: _tabController,
                   children: [
+                    //edible list
                     FutureBuilder(
                       future: getPlantList.getEdiblePlantList(),
                       builder: (context, snapshot) {
@@ -185,156 +186,65 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
                         if (!snapshot.hasData) {
                           return Text("Error: Ther is no data");
                         }
-                        return Column(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.only(
-                                  top: 16.8, left: 28.8, right: 28.8),
-                              child: Row(
-                                crossAxisAlignment: CrossAxisAlignment.end,
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text(
-                                    "Edible Plants",
-                                    style: GoogleFonts.amaranth(
-                                      fontSize: 32.6,
-                                      fontWeight: FontWeight.w600,
-                                      color:
-                                          const Color.fromARGB(255, 38, 36, 36),
-                                    ),
-                                  ),
-                                  Text(
-                                    "see all",
-                                    style: GoogleFonts.roboto(
-                                      fontSize: 18.2,
-                                      fontWeight: FontWeight.w400,
-                                      color: const Color.fromARGB(
-                                          255, 81, 177, 255),
-                                    ),
-                                  )
-                                ],
-                              ),
-                            ),
-                            Container(
-                              height: 328.5,
-                              margin: EdgeInsets.only(top: 18.8),
-                              child: PageView(
-                                physics: BouncingScrollPhysics(),
-                                controller: _pageController,
-                                scrollDirection: Axis.horizontal,
-                                children: List.generate(
-                                    getPlantList.getEdibleList.length,
-                                    (index) => GestureDetector(
-                                          onTap: () {},
-                                          child: Container(
-                                            margin: const EdgeInsets.only(
-                                                right: 28.8),
-                                            decoration: BoxDecoration(
-                                                borderRadius:
-                                                    BorderRadius.circular(9.6),
-                                                image: DecorationImage(
-                                                    image:
-                                                        CachedNetworkImageProvider(
-                                                            getPlantList
-                                                                .getEdibleList[
-                                                                    index]
-                                                                .original_url),
-                                                    fit: BoxFit.cover)),
-                                            child: Stack(
-                                              children: [
-                                                Positioned(
-                                                  bottom: 19.2,
-                                                  left: 19.2,
-                                                  child: ClipRRect(
-                                                    borderRadius:
-                                                        BorderRadius.circular(
-                                                            4.8),
-                                                    child: BackdropFilter(
-                                                      filter: ImageFilter.blur(
-                                                          sigmaY: 19.2,
-                                                          sigmaX: 19.2),
-                                                      child: Container(
-                                                        height: 36,
-                                                        padding:
-                                                            const EdgeInsets
-                                                                    .only(
-                                                                left: 16.72,
-                                                                right: 14.4),
-                                                        alignment: Alignment
-                                                            .centerLeft,
-                                                        child: Text(
-                                                          getPlantList
-                                                              .getEdibleList[
-                                                                  index]
-                                                              .common_name,
-                                                          style: GoogleFonts
-                                                              .roboto(
-                                                                  fontWeight:
-                                                                      FontWeight
-                                                                          .w700,
-                                                                  color: Colors
-                                                                      .white,
-                                                                  fontSize:
-                                                                      16.8),
-                                                        ),
-                                                      ),
-                                                    ),
-                                                  ),
-                                                )
-                                              ],
-                                            ),
-                                          ),
-                                        )),
-                              ),
-                            )
-                          ],
-                        );
+                        return fetchColumnList(
+                            "Edible Plants",
+                            getPlantList.getEdibleList.length,
+                            getPlantList.getEdibleList);
                       },
                     ),
-                    // SizedBox(
-                    //   child: fetchFuture(
-                    //       getPlantList.getEdiblePlantList(),
-                    //       "Edible Plants",
-                    //       getPlantList.getEdibleList.length,
-                    //       getPlantList.getEdibleList),
-                    // ),
+
+                    // poisonus list
+
                     FutureBuilder(
-                        future: getPlantList.getPoisonusPlantList(),
-                        builder: (context, snapshot) {
-                          if (snapshot.connectionState ==
-                              ConnectionState.waiting) {
-                            return Container(
-                              padding: const EdgeInsets.all(120.8),
-                              child: const CircularProgressIndicator.adaptive(
-                                backgroundColor: Colors.white,
-                              ),
-                            ); // Tampilkan loading spinner saat proses fetch data masih berjalan
-                          }
-                          if (snapshot.hasError) {
-                            return Text("Error: ${snapshot.error}");
-                          }
-                          if (!snapshot.hasData) {
-                            return Text("Error: Ther is no data");
-                          }
-                          return fetchColumnList(
-                              "Poisonus Plants",
-                              getPlantList.getPoisonusList.length,
-                              getPlantList.getPoisonusList);
-                        }),
-                    // SizedBox(
-                    //   child: fetchFuture(
-                    //       getPlantList.getPoisonusPlantList(),
-                    //       "Poisonus Plants",
-                    //       getPlantList.getPoisonusList.length,
-                    //       getPlantList.getPoisonusList),
-                    // ),
-                    SizedBox(
-                      child: fetchFuture(
-                          getPlantList.getIndoorPlantList(),
-                          "Indoor Plants",
-                          getPlantList.getIndoorList.length,
-                          getPlantList.getIndoorList),
+                      future: getPlantList.getPoisonusPlantList(),
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState ==
+                            ConnectionState.waiting) {
+                          return Container(
+                            padding: const EdgeInsets.all(120.8),
+                            child: const CircularProgressIndicator.adaptive(
+                              backgroundColor: Colors.white,
+                            ),
+                          ); // Tampilkan loading spinner saat proses fetch data masih berjalan
+                        }
+                        if (snapshot.hasError) {
+                          return Text("Error: ${snapshot.error}");
+                        }
+                        if (!snapshot.hasData) {
+                          return Text("Error: Ther is no data");
+                        }
+                        return fetchColumnList(
+                            "Poisonus Plants",
+                            getPlantList.getPoisonusList.length,
+                            getPlantList.getPoisonusList);
+                      },
+                    ),
+
+                    // indoor list
+
+                    FutureBuilder(
+                      future: getPlantList.getIndoorPlantList(),
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState ==
+                            ConnectionState.waiting) {
+                          return Container(
+                            padding: const EdgeInsets.all(120.8),
+                            child: const CircularProgressIndicator.adaptive(
+                              backgroundColor: Colors.white,
+                            ),
+                          ); // Tampilkan loading spinner saat proses fetch data masih berjalan
+                        }
+                        if (snapshot.hasError) {
+                          return Text("Error: ${snapshot.error}");
+                        }
+                        if (!snapshot.hasData) {
+                          return Text("Error: Ther is no data");
+                        }
+                        return fetchColumnList(
+                            "Indoor Plants",
+                            getPlantList.getIndoorList.length,
+                            getPlantList.getIndoorList);
+                      },
                     ),
                   ],
                 ),
@@ -347,110 +257,6 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
   }
 
   //don't reapeate your code
-
-  fetchFuture(future, titleName, plantCategoryLength, plantCategory) {
-    return FutureBuilder(
-      future: future,
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return Container(
-            padding: const EdgeInsets.all(120.8),
-            child: const CircularProgressIndicator.adaptive(
-              backgroundColor: Colors.white,
-            ),
-          ); // Tampilkan loading spinner saat proses fetch data masih berjalan
-        }
-        if (snapshot.hasError) {
-          return Text("Error: ${snapshot.error}");
-        }
-        if (!snapshot.hasData) {
-          return Text("Error: Ther is no data");
-        }
-        return Column(
-          children: [
-            Padding(
-              padding:
-                  const EdgeInsets.only(top: 16.8, left: 28.8, right: 28.8),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    titleName,
-                    style: GoogleFonts.amaranth(
-                      fontSize: 32.6,
-                      fontWeight: FontWeight.w600,
-                      color: const Color.fromARGB(255, 38, 36, 36),
-                    ),
-                  ),
-                  Text(
-                    "see all",
-                    style: GoogleFonts.roboto(
-                      fontSize: 18.2,
-                      fontWeight: FontWeight.w400,
-                      color: const Color.fromARGB(255, 81, 177, 255),
-                    ),
-                  )
-                ],
-              ),
-            ),
-            Container(
-              height: 328.5,
-              margin: EdgeInsets.only(top: 18.8),
-              child: PageView(
-                physics: BouncingScrollPhysics(),
-                controller: _pageController,
-                scrollDirection: Axis.horizontal,
-                children: List.generate(
-                    plantCategoryLength,
-                    (index) => GestureDetector(
-                          onTap: () {},
-                          child: Container(
-                            margin: const EdgeInsets.only(right: 28.8),
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(9.6),
-                                image: DecorationImage(
-                                    image: CachedNetworkImageProvider(
-                                        plantCategory[index].original_url),
-                                    fit: BoxFit.cover)),
-                            child: Stack(
-                              children: [
-                                Positioned(
-                                  bottom: 19.2,
-                                  left: 19.2,
-                                  child: ClipRRect(
-                                    borderRadius: BorderRadius.circular(4.8),
-                                    child: BackdropFilter(
-                                      filter: ImageFilter.blur(
-                                          sigmaY: 19.2, sigmaX: 19.2),
-                                      child: Container(
-                                        height: 36,
-                                        padding: const EdgeInsets.only(
-                                            left: 16.72, right: 14.4),
-                                        alignment: Alignment.centerLeft,
-                                        child: Text(
-                                          plantCategory[index].common_name,
-                                          style: GoogleFonts.roboto(
-                                              fontWeight: FontWeight.w700,
-                                              color: Colors.white,
-                                              fontSize: 16.8),
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                )
-                              ],
-                            ),
-                          ),
-                        )),
-              ),
-            )
-          ],
-        );
-      },
-    );
-  }
-
   fetchColumnList(titleName, plantCategoryLength, plantCategory) {
     return Column(
       children: [
