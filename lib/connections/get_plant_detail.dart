c349e39e1862db807eef169c32a9a5e343cf0bce
Ardiantohendy <bayuardianto421@gmail.com>
2023-06-19 16:18:56 +0700

    new commit

---
+++ lib/connections/get_plant_detail.dart
@@ -0,0 +1,3 @@
+class GetPlantDetail {
+  getPlantDetail() async {}
+}
--- lib/screen/selected_plant_screen.dart
+++ lib/screen/selected_plant_screen.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:plant_app/models/plant_detail_model.dart';
+import 'package:plant_app/repository/plant_detail_repository.dart';
 
 class SelectedPlantScreen extends StatefulWidget {
   int id;
+
   SelectedPlantScreen({required this.id});
 
   @override
@@ -11,15 +14,18 @@ class SelectedPlantScreen extends StatefulWidget {
 }
 
 class _SelectedPlantScreenState extends State<SelectedPlantScreen> {
+  PlantDetailRepository plantDetailRepository = PlantDetailRepository();
+
   @override
   Widget build(BuildContext context) {
+    // PlantDetailModel plantDetailModel =
+    //     plantDetailRepository.fetchPlantDetail(widget.id);
+
     return Scaffold(
       body: SafeArea(
           child: Container(
         decoration: const BoxDecoration(color: Colors.white),
-        child: ListView(
-          children: [Text(widget.id.toString())],
-        ),
+        child: Text(widget.id.toString()),
       )),
     );
   }
